83bf47977df205ebc87bbadb72763aff995a9084
Tonton-Blax <redval@gmail.com>
2021-09-29 13:42:25 +0200

    banner gpdr + test preview

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,10 +1,36 @@
 <script>
 	import Nav from '../components/Nav.svelte'
+	import { onMount } from 'svelte'
 	import Footer from '../components/Footer.svelte'
 	import Transition from '../components/Transition.svelte';
+	import GdprBanner from '@beyonk/gdpr-cookie-consent-banner'
+	import '../styles/cookies.css' // import optional styles
 	export let segment;
-</script>
 
+	function initAnalytics () {
+		console.log("kikou lol")
+  	}
+
+</script>
+<GdprBanner cookieName="foo"
+	heading="EDRA respecte vos données : "
+	acceptLabel="Accepter"
+	settingsLabel="Paramètres des cookies"
+	closeLabel="Fermer"
+	description="Nous utilisons des cookies, afin d'améliorer la performance de votre navigation, et mesurer l'audience du site. Vous pouvez, à tout moment, désactiver ces cookies"
+	choices = {{
+		analytics : {
+			label : "Cookies d'analyse statistique",
+			description : "Utilisé par google Analytics afin de mesurer l'audience de notre site"
+		},
+		necessary : {
+			label : "Cookies requis",
+			description : "Cookie pour contrôle des autres cookies, ne peut être désactivé"
+		},
+		tracking : false,
+		marketing : false
+	}}
+	on:analytics={initAnalytics} />
 <Nav {segment} />
 <body>
 <div class="container">
