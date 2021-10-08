bce47694708b37258a2543ae302cfa3f0a21e952
Tonton-Blax <redval@gmail.com>
2021-10-08 18:30:14 +0200

    mentioNS

--- src/lib/layout/Footer.svelte
+++ src/lib/layout/Footer.svelte
@@ -39,7 +39,7 @@
 		<div class="column is-half">
 			<div class="edra-footer has-text-white">
                 <p class="is-size-6" style="align-self:flex-start   ">© EDRA 2021<br>
-                    <a href='/mentions'>Mention légales RGPD</a>
+                    <a href='/mentions'>Mentions légales RGPD</a>
                 </p>
 				<h1 class="is-size-6" style="align-self:flex-start   ">© EDRA 2021<br>
                     Réalisation : Maxime et Tibo Redval
